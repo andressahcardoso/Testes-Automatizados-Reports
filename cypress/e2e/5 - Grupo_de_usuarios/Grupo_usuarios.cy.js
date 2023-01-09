45e76bd8c230b3423f72a4d91121e768b14741a3
amanda-benites <amandabenites2005@gmail.com>
2023-01-09 11:39:27 -0300

    tentando arrumar problema nos testes de grupo de usuario

--- cypress/e2e/5 - Grupo_de_usuarios/Grupo_usuarios.cy.js
+++ cypress/e2e/5 - Grupo_de_usuarios/Grupo_usuarios.cy.js
@@ -3,21 +3,24 @@ describe('Testes em grupos de usuario', () => {
     beforeEach(() => {
         cy.visit('/')
     });
-    it('Criando um grupo de usuarios', () => {
+    it.only('Criando um grupo de usuarios', () => {
         cy.login(Cypress.env('userName'), Cypress.env('password'));
         cy.openOptions()
         cy.contains('span', 'Novo grupo de usuários').click(); 
-        cy.createUserG(Cypress.env('rel_title_id'), Cypress.env('rel_title'), Cypress.env('email'));
+        cy.createUserG(Cypress.env('rel_title'), Cypress.env('rel_title_id'), Cypress.env('email'));
         cy.get('button').contains('span', 'Próximo').click();
         cy.get('mat-checkbox[id="mat-checkbox-8"]').click();
         cy.get('button').contains('span', 'Próximo').click({force: true});
         cy.get('button').contains('span', 'Próximo').click({force: true});
-        cy.get('div[id="mat-tab-label-1-2"]').click();
-        cy.get('div[class="mat-form-field-infix ng-tns-c76-92"]').type('Teste 16/09 (Gauge)');
-        cy.get('#mat-checkbox-520 > .mat-checkbox-layout > .mat-checkbox-inner-container').click(); //Utilizado o Selector Playground​ do Cypress
+        cy.get('div[id="mat-tab-label-1-2"]').click(); 
         cy.get('button').contains('span', 'Finalizar e salvar').click();
         cy.confirmCreate();
         cy.existsGUs(Cypress.env('rel_title_id'))
+    });
+
+    it.only('Editando grupo de usuarios', () => {
+        cy.login(Cypress.env('userName'), Cypress.env('password'));
+        cy.GUsGerenEdit(Cypress.env('rel_title'));
     });
 });
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -109,9 +109,22 @@ Cypress.Commands.add('GUsGeren', (GUsearch) => {
     cy.get('#mat-input-9').type(GUsearch); //Utilizado o Selector Playground do Cypress
     cy.get('img[class*="ic-search"]').click();
 })
+Cypress.Commands.add('GUsGerenEdit', (GUsearch) => {
+    cy.get('.mat-toolbar-row > :nth-child(6)').click(); //Utilizado o Selector Playground do Cypress
+    cy.contains('span', ' Gerenciadores ').click();
+    cy.wait(500);
+    cy.get('div[class^="mat-ripple"]').contains('span', 'Grupo de usuários').click()
+    cy.get('input[data-placeholder="Procurar"]').type(GUsearch);
+    cy.get('img[class*="ic-search"]').click();
+})
 
 Cypress.Commands.add('existsGUs', (gName) => {
     cy.wait(600);
     cy.GUsGeren(gName);
+    cy.contains('div', gName).should('be.visible');
+})
+Cypress.Commands.add('existsGUsEdit', (gName) => {
+    cy.wait(600);
+    cy.GUsGerenEdit(gName);
     cy.contains('div', gName).should('be.visible');
 })
