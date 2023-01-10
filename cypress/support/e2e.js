8f8cdddf6b01ab89250b54e1ade9fa3b4e9982c5
Andressa Cardoso <andressa.cardoso@ska.com.br>
2023-01-10 10:32:51 -0300

    Criação de Sinótico

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,6 +16,7 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import './general-commands'
+import 'cypress-file-upload'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
